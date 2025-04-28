724e452073e3b42e82742ef34175f267d8c5bf43
alessio <alessio.giorgetti@gmail.com>
2025-04-28 10:57:23 +0200

    - fixed version of poisson-scapy - more info in README file

--- lab-04/poisson-scapy.py
+++ lab-04/poisson-scapy.py
@@ -2,10 +2,13 @@ import random
 import math
 import time
 import logging
-from scapy.all import *
+from scapy.all import send
+from scapy.layers.inet import IP
+from scapy.layers.l2 import Ether
+from scapy.packet import Raw
 
-lambda_arrival = 0.1  # Intensity of arrivals (packets per second)
-lambda_length = 0.002  # Intensity of length (packets per byte) thus average length 1/lambda_length
+lambda_arrival = 10  # Intensity of arrivals (packets per second)
+lambda_length = 0.1  # Intensity of length (packets per byte) thus average length 1/lambda_length
 
 def generate_packet_length():
     return int(-math.log(1.0 - random.random()) / lambda_length)
@@ -18,25 +21,28 @@ def generate_packets():
     generated_bytes = 0
 
     while True:
+	# Interarrival time
         interarrival_time = random.expovariate(lambda_arrival)
         time.sleep(interarrival_time)
-        
-        packet_length = 4
-        packet_data = bytearray(random.getrandbits(8) for _ in range(packet_length))
 
-        # UDP packet creation with SCAPY
-        packet = IP(dst="10.0.2.2")/Ether(type=0x0800)/Raw(load=packet_data)
+        # Packet size - float value is truncated (+1 to compensate), then effective length is higher than theory
+        packet_length = random.expovariate(lambda_length)
+        packet_bytes = int(packet_length)
+
+        # IP packet creation with SCAPY - proto is required otherwise wireshark does not recognize the packet
+        # Python strings with b"..." are interpreted as strings of bytes
+        packet = IP(src="10.0.2.15", dst="10.0.2.2", proto=253)/Raw(load=b"A" * packet_bytes)
         
         # Send packet using SCAPY
         send(packet, iface="enp0s3", verbose=False)
 
         generated_packets = generated_packets + 1
-        generated_bytes = generated_bytes + packet_length
+        generated_bytes = generated_bytes + packet_bytes
         generated_average_length = str("{:.2f}".format(generated_bytes / generated_packets))
 
-        print("Actual packet " + str(packet_length) + " bytes - current average length: " + generated_average_length + " bytes")
+        print("Actual packet " + str(packet_bytes) + " bytes - current average length: " + generated_average_length + " bytes")
 
 # Avvia la generazione dei pacchetti
-print("Expected average packet length:", 1/lambda_length)
+print("Expected average packet length (theoretical value):", 1/lambda_length)
 generate_packets()
 
